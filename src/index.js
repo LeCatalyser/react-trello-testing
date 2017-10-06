7fe80476a60ad4502c66d17b2391c053d25b6fed
Luisa Salcedo <profe.salcedo@gmail.com>
2017-10-06 13:13:32 -0500

    first deploy to netlify

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Board from './components/board';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import Board from "./components/board";
+import "./index.css";
 
-ReactDOM.render(
-  <Board />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Board />, document.getElementById("root"));
